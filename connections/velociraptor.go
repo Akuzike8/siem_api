ffb7ab7668caff27c5fff1071b18d97525331ba3
Akuzike Nchembe <akuzikenchembe8@gmail.com>
2025-02-13 15:26:29 +0200

    update

--- connections/velociraptor.go
+++ connections/velociraptor.go
@@ -2,115 +2,30 @@ package connections
 
 import (
 	"context"
-	"crypto/tls"
-	"crypto/x509"
-	"io"
-	"log"
-	"os"
-
-	"github.com/Akuzike8/siem_api/dto"
-	"github.com/Velocidex/yaml/v2"
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/credentials"
+	"fmt"
+	"os/exec"
+	"time"
 )
 
-// Function to load the configuration file
-func LoadConfig(configPath string) (*dto.VelociraptorConfig, error) {
-	// Read the configuration file
-	y, err := os.ReadFile(configPath)
-	if err != nil {
-		return nil, err
-	}
-
-	// Define the struct to store the config
-	var config ApiClientConfig
-	// Unmarshal the YAML file into the config struct
-	err = yaml.Unmarshal(y, &config)
-	if err != nil {
-		return nil, err
-	}
-
-	// Return a new config object from the DTO package
-	return &dto.VelociraptorConfig{
-		ClientCert:          config.ClientCert,
-		ClientPrivateKey:    config.ClientPrivateKey,
-		CaCertificate:       config.CaCertificate,
-		ApiConnectionString: config.ApiConnectionString,
-	}, nil
-}
-
-// Function to establish a gRPC connection with TLS credentials
-func CreateGRPCConnection(config *dto.VelociraptorConfig) (*grpc.ClientConn, error) {
-	// Load the certificates
-	cert, err := tls.X509KeyPair([]byte(config.ClientCert), []byte(config.ClientPrivateKey))
-	if err != nil {
-		return nil, err
-	}
-
-	// Prepare the CA certificate pool
-	caCertPool := x509.NewCertPool()
-	caCertPool.AppendCertsFromPEM([]byte(config.CaCertificate))
-
-	// Create the TLS credentials
-	tlsCredentials := credentials.NewTLS(&tls.Config{
-		Certificates: []tls.Certificate{cert},
-		RootCAs:      caCertPool,
-		ServerName:   "VelociraptorServer",
-	})
-
-	// Establish the gRPC connection
-	conn, err := grpc.Dial(config.ApiConnectionString, grpc.WithTransportCredentials(tlsCredentials))
-	if err != nil {
-		return nil, err
-	}
-
-	return conn, nil
-}
-
-// Function to execute a VQL query and return the query client
-func ExecuteVQLQuery(conn *grpc.ClientConn, query string) (API_QueryClient, error) {
-	client := NewAPIClient(conn)
-
-	// Create the VQL request
-	request := &VQLCollectorArgs{
-		Query: []*VQLRequest{
-			{
-				Name: "Query",
-				VQL:  query,
-			},
-		},
-	}
-
-	// Create a context and execute the query
-	ctx, cancel := context.WithCancel(context.Background())
+func ExecuteVQLQuery(query string) {
+	// Create a context with a timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
-	// Execute the query and return the client
-	queryClient, err := client.Query(ctx, request)
+	cmd := exec.CommandContext(ctx,"velociraptor","--api_config", "config/api.config.yaml", "query", query, "--format", "jsonl","|", "jq")
+
+	// Run the Python script
+	output, err := cmd.CombinedOutput()
 	if err != nil {
-		return nil, err
+		// Handle timeout or other errors
+		if ctx.Err() == context.DeadlineExceeded {
+			fmt.Println("Python script timed out")
+		} else {
+			fmt.Println("Error executing Python script:", err)
+		}
+		return
 	}
 
-	return queryClient, nil
-}
-
-// Function to process and log the responses
-func ProcessResponses(queryClient API_QueryClient) error {
-	for {
-		resp, err := queryClient.Recv()
-		if err != nil {
-			if err == io.EOF {
-				break // End of the stream, nothing more to receive
-			}
-			return err // Return error instead of logging panic
-		}
-
-		// Log the response or log messages
-		if resp.Response != "" {
-			log.Print("Response: ", resp.Response)
-		} else if resp.Log != "" {
-			log.Print(resp.Log)
-		}
-	}
-	return nil // Return nil if no errors occurred
+	// Print the output
+	fmt.Println("Output from Python script:", string(output))
 }
--- handles/velociraptor.go
+++ handles/velociraptor.go
@@ -2,7 +2,6 @@ package handles
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"log"
@@ -29,33 +28,10 @@ func VelociraptorQuarantine(w http.ResponseWriter, r *http.Request){
 		log.Fatal("Config path is not provided")
 	}
 
-	// Load configuration
-	config, err := connections.LoadConfig(configPath)
-	if err != nil {
-		log.Fatalf("Failed to load config: %v", err)
-	}
-
-	// Establish a gRPC connection
-	conn, err := connections.CreateGRPCConnection(config)
-	if err != nil {
-		log.Fatalf("Failed to create gRPC connection: %v", err)
-	}
-	defer conn.Close()
-
 	// Execute a VQL query
 	query := "SELECT * FROM info()"
 
-	queryClient, err := connections.ExecuteVQLQuery(conn, query)
-	if err != nil {
-        http.Error(w, fmt.Sprintf("Error executing VQL query: %v", err), http.StatusInternalServerError)
-        return
-    }
-
-    // Process the responses
-    if err := connections.ProcessResponses(queryClient); err != nil {
-        http.Error(w, fmt.Sprintf("Error processing responses: %v", err), http.StatusInternalServerError)
-        return
-    }
+	connections.ExecuteVQLQuery(query)
 
 	// Return success response
     w.WriteHeader(http.StatusOK)
@@ -77,23 +53,4 @@ func VelociraptorUnQuarantine(w http.ResponseWriter, r *http.Request){
 		log.Fatal("Config path is not provided")
 	}
 
-	// Load configuration
-	config, err := connections.LoadConfig(configPath)
-	if err != nil {
-		log.Fatalf("Failed to load config: %v", err)
-	}
-
-	// Establish a gRPC connection
-	conn, err := connections.CreateGRPCConnection(config)
-	if err != nil {
-		log.Fatalf("Failed to create gRPC connection: %v", err)
-	}
-	defer conn.Close()
-
-	// Execute a VQL query
-	query := "SELECT * FROM info()"
-	_, err = connections.ExecuteVQLQuery(conn, query)
-	if err != nil {
-		log.Fatalf("Error executing VQL query: %v", err)
-	}
 }
